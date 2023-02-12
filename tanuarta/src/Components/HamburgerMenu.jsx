7d2206d16f906bdf582a3aa84b3b4e9610e63764
tanuarta <rtanuarta@gmail.com>
2023-02-12 21:19:09 +1100

    Animation for hamburg menu

--- tanuarta/src/Components/HamburgerMenu.jsx
+++ tanuarta/src/Components/HamburgerMenu.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
 import styles from './HamburgerMenu.module.scss'
+import { motion, useAnimationControls } from "framer-motion"
 
 
 const HambugerMenu = ({ menuShown }) => {
 
-  return (<>
+  const animations = {
+    initial: { opacity: 0, x: 2000},
+    animate: { opacity: 1, x:0, transition: {duration: 0.5}},
+    exit: { opacity: 0, x: 2000, transition: {delay: 0.5, duration: 0.5}},
+  }
   
-    <div className={`${styles.box} ${menuShown ? '' : styles.hidden}`}>
+  const childrenVariant = {
+    initial: { opacity: 0, y: 70},
+    animate: { opacity: 1, y: 0, transition: {delay: 0.5, duration: 0.5}},
+    exit: {opacity: 0, y: 70, transition: {duration: 0.5}},
+  };
+
+  return (<>
+    <motion.div 
+      className={styles.box} 
+      variants={animations}
+      initial='initial'
+      animate={menuShown ? 'animate' : 'exit'}
+      exit='exit'
+    >
       <div className={styles.main}>
-        <span className={styles.text}>tanuarta</span>
+        <motion.span 
+          initial='initial'
+          animate={menuShown ? 'animate' : 'exit'}
+          exit='exit'
+          variants={childrenVariant} 
+          className={styles.menuText}
+        >
+          Home
+        </motion.span>
         
-        <span className={styles.menuText}>Home</span>
+        <motion.span 
+          initial='initial'
+          animate={menuShown ? 'animate' : 'exit'}
+          exit='exit'
+          variants={childrenVariant} 
+          className={styles.menuText}
+        >
+          Projects
+        </motion.span>
       </div>
-    </div>
+    </motion.div>
   
   </>)
 }
