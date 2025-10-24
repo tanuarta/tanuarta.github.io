1948433452c839956e4f20153214b48cb8bf669f
Roan Tanuarta <rtanuarta@gmail.com>
2025-10-24 22:58:24 +1100

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import About from './Pages/About';
 import Unknown from './Pages/Unknown';
 import Blog from './Pages/Blog';
 import TicTacToe from './Pages/TicTacToe';
-import pdffile from './Media/Bp_Eddy_Tjahja_Funeral_Booklet.pdf';
+import DisplayPDF from './Pages/DisplayPDF';
 
 const Main = ({ setCursorMode }) => {
   return (
@@ -16,7 +16,7 @@ const Main = ({ setCursorMode }) => {
       <Route path='/about' element={<About setCursorMode={setCursorMode} />}></Route>
       <Route path='/blog' element={<Blog setCursorMode={setCursorMode} />}></Route>
       <Route path="/tictactoe" element={<TicTacToe setCursorMode={setCursorMode} />} />
-      <Route path="/funeralbooklet" element={pdffile} target="_blank" rel="noopener noreferrer" download />
+      <Route path="/funeralbooklet" element={DisplayPDF} setCursorMode={setCursorMode} />
       <Route path="*" element={<Unknown setCursorMode={setCursorMode} />} />
     </Routes>
   );
