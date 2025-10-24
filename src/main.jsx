82e050040f143cf112e6354ba7151155c9beea74
Roan Tanuarta <rtanuarta@gmail.com>
2025-10-24 22:39:32 +1100

    added route

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import About from './Pages/About';
 import Unknown from './Pages/Unknown';
 import Blog from './Pages/Blog';
 import TicTacToe from './Pages/TicTacToe';
+import pdf from './Media/Bp Eddy Tjahja Funeral Booklet.pdf'
 
 const Main = ({ setCursorMode }) => {
   return (
@@ -15,6 +16,7 @@ const Main = ({ setCursorMode }) => {
       <Route path='/about' element={<About setCursorMode={setCursorMode} />}></Route>
       <Route path='/blog' element={<Blog setCursorMode={setCursorMode} />}></Route>
       <Route path="/tictactoe" element={<TicTacToe setCursorMode={setCursorMode} />} />
+      <Route path="/funeralbooklet" element={pdf} target="_blank" rel="noopener noreferrer" download />
       <Route path="*" element={<Unknown setCursorMode={setCursorMode} />} />
       
     </Routes>
